5954fa8ad55f73868d9e2cc870f84c525bac101d
Hernan <hernan.lopez@gmail.com>
2024-10-22 14:44:31 -0300

    geonode-cloud-core/geonode/base/api/views.py

--- geonode-cloud-core/geonode/base/api/views.py
+++ geonode-cloud-core/geonode/base/api/views.py
@@ -587,6 +587,7 @@ class ResourceBaseViewSet(DynamicModelViewSet):
         permission_classes=[IsAuthenticated],
     )
     def resource_service_permissions(self, request, pk, *args, **kwargs):
+
         """Instructs the Async dispatcher to execute a 'DELETE' or 'UPDATE' on the permissions of a valid 'uuid'
 
             - GET input_params: {
@@ -619,24 +620,39 @@ class ResourceBaseViewSet(DynamicModelViewSet):
         Sample Requests:
 
         - Removes all the permissions (except owner and admin ones) from a Resource:
+
+            .. code-block:: bash
+
             curl -v -X DELETE -u admin:admin -H "Content-Type: application/json" http://localhost:8000/api/v2/resources/<id>/permissions
 
         - Changes the owner of a Resource:
+
+            .. code-block:: bash
+
             curl -u admin:admin --location --request PUT 'http://localhost:8000/api/v2/resources/<id>/permissions' \
                 --header 'Content-Type: application/json' \
                 --data-raw '{"groups": [],"organizations": [],"users": [{"id": 1001,"permissions": "owner"}]}'
 
         - Assigns View permissions to some users:
+
+            .. code-block:: bash
+
             curl -u admin:admin --location --request PUT 'http://localhost:8000/api/v2/resources/<id>/permissions' \
                 --header 'Content-Type: application/json' \
                 --data-raw '{"groups": [],"organizations": [],"users": [{"id": 1000,"permissions": "view"}]}'
 
         - Assigns View permissions to anyone:
+
+            .. code-block:: bash
+
             curl -u admin:admin --location --request PUT 'http://localhost:8000/api/v2/resources/<id>/permissions' \
                 --header 'Content-Type: application/json' \
                 --data-raw '{"groups": [],"organizations": [],"users": [{"id": -1,"permissions": "view"}]}'
 
         - Assigns View permissions to anyone and edit permissions to a Group on a Dataset:
+
+            .. code-block:: bash
+
             curl -u admin:admin --location --request PUT 'http://localhost:8000/api/v2/resources/<id>/permissions' \
                 --header 'Content-Type: application/json' \
                 --data-raw '{"groups": [{"id": 1,"permissions": "manage"}],"organizations": [],"users": [{"id": -1,"permissions": "view"}]}'
